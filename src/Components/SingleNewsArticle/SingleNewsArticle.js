c9f627117a33dd5a1869320782a5ca18438d74a2
Jocelyn Wensloff <jewensloff@gmail.com>
2023-10-30 16:18:08 -0600

    feat: Add a border to the cards

--- src/Components/SingleNewsArticle/SingleNewsArticle.js
+++ src/Components/SingleNewsArticle/SingleNewsArticle.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import '../SingleNewsArticle/SingleNewsArticle.scss'
 function SingleNewsArticle({
   id,
   title,
@@ -8,12 +8,12 @@ function SingleNewsArticle({
   description,
   published,
 }) {
-  return <div id={id}>
+  return <div id={id} className='news-card'>
     <h2>{title}</h2>
     <h3>{description}</h3>
     <h3>Written by: {author}</h3>
     <h3>Published: {published}</h3>
-    <img src={image}></img>
+    <img src={image} className='news-img'></img>
   </div>;
 }
 
