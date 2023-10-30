690d0ecf37ee8783c8ed3751189e7e6f44681786
Jocelyn Wensloff <jewensloff@gmail.com>
2023-10-30 16:03:23 -0600

    feat: Display mock data to the DOM

--- src/Components/NewsContainer/NewsContainer.js
+++ src/Components/NewsContainer/NewsContainer.js
@@ -6,10 +6,13 @@ function NewsContainer({articles}) {
   let allArticles = articles.map(article => {
     return (
       <SingleNewsArticle 
+      key={article.url}
       id={article.url}
       title={article.title}
       image={article.urlToImage}
-      
+      author={article.author}
+      description={article.description}
+      published={article.publishedAt}
       /> 
     )
   })
--- src/Components/SingleNewsArticle/SingleNewsArticle.js
+++ src/Components/SingleNewsArticle/SingleNewsArticle.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-function SingleNewsArticle() {
-  return (
-    <div>
-      single card
-    </div>
-  )
+function SingleNewsArticle({
+  id,
+  title,
+  image,
+  author,
+  description,
+  published,
+}) {
+  return <div id={id}>
+    <h2>{title}</h2>
+    <h3>{description}</h3>
+    <h3>Written by: {author}</h3>
+    <h3>Published: {published}</h3>
+    <img src={image}></img>
+  </div>;
 }
 
-export default SingleNewsArticle
+export default SingleNewsArticle;
