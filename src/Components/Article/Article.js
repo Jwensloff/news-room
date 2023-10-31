bbb7b1a127ddb73564c74f554049da8b39cebf39
Jocelyn Wensloff <jewensloff@gmail.com>
2023-10-31 11:47:01 -0600

    feat: Add styling to Nav Bar

--- src/Components/Article/Article.js
+++ src/Components/Article/Article.js
@@ -4,16 +4,16 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 function Article({ article }) {
-  // const { title } = useParams();
   console.log('here', article);
   const { id, title, urlToImage, author, content, publishedAt } = article;
   return (
     <div className='article'>
       <h1>{title}</h1>
       <img src={urlToImage} className='article-img'></img>
-      <h3>Written by: {author}</h3>
-      <h3>{publishedAt}</h3>
-      <h3>{content}</h3>
+      <h3 className='article-author'>Written by: {author}</h3>
+      <h3 className='article-published'>Published on: {publishedAt}</h3>
+      <span></span>
+      <h3 className='article-content'>{content}</h3>
     </div>
   );
 }
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './NavBar.scss';
+import { NavLink } from 'react-router-dom';
 
 function NavBar() {
   return (
     <header className='header'>
-      <h1>News Room</h1>
+        <h1>News Room</h1>
+        <NavLink to={'/'} className='home-btn'>
+          Home
+        </NavLink>
     </header>
   );
 }
