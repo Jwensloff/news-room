fd760d089025b18898b4c580e9dec132331d39d8
Jocelyn Wensloff <jewensloff@gmail.com>
2023-10-31 14:15:41 -0600

    feat: Format dates

--- src/Components/Article/Article.js
+++ src/Components/Article/Article.js
@@ -2,16 +2,21 @@ import './Article.scss';
 
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import dayjs from 'dayjs';
 
 function Article({ article }) {
-  console.log('here', article);
   const { id, title, urlToImage, author, content, publishedAt } = article;
+
+  const originalDate = publishedAt;
+  const formattedDate = dayjs(originalDate).format('MMMM D, YYYY, h:mm A');
+
+  console.log(formattedDate);
   return (
     <div className='article'>
       <h1>{title}</h1>
       <img src={urlToImage} className='article-img'></img>
       <h3 className='article-author'>Written by: {author}</h3>
-      <h3 className='article-published'>Published on: {publishedAt}</h3>
+      <h3 className='article-published'>{formattedDate}</h3>
       <span></span>
       <h3 className='article-content'>{content}</h3>
     </div>
--- src/Components/SingleNewsArticle/SingleNewsArticle.js
+++ src/Components/SingleNewsArticle/SingleNewsArticle.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../SingleNewsArticle/SingleNewsArticle.scss';
+import dayjs from 'dayjs';
+
 function SingleNewsArticle({
   id,
   title,
@@ -9,12 +11,15 @@ function SingleNewsArticle({
   published,
   handleClick,
 }) {
+  const originalDate = published;
+  const formattedDate = dayjs(originalDate).format('MMMM D, YYYY, h:mm A');
+
   return (
     <div id={id} className='news-card' onClick={() => handleClick(title)}>
       <img src={image} className='news-img'></img>
       <h2>{title}</h2>
       <h3 className='author-text'>Written by: {author}</h3>
-      <h3 className='published-text'>Published: {published}</h3>
+      <h3 className='published-text'>{formattedDate}</h3>
       <h3>{description}</h3>
     </div>
   );
